bde577b3736fdbb441fffea26b802d221a6547e3
Bilal Fazlani <bfazlani@seek.com.au>
2016-02-04 00:52:25 +0530

    initial

---
+++ src/JsonServer/Program.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.Dnx.Runtime.Common.CommandLine;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace JsonServer
+{
+    public class Program
+    {
+        public async Task<int> Main(string[] args)
+        {
+            var app = new CommandLineApplication
+            {
+                Name = "JSONServer",
+                Description = "Creates a server that returns static server",
+                FullName = "JSON Server",
+            };
+
+            app.Command("start", command =>
+            {
+                command.Description = "Starts the json file server";
+                var fileOption = command.Option("-f|--file <FILE>", "json file to read", CommandOptionType.SingleValue);
+                var urlOption = command.Option("-u|--url <URL>", "url to host the service", CommandOptionType.SingleValue);
+
+                command.OnExecute(() =>
+                {
+                    if (!fileOption.HasValue())
+                    {
+                        Console.WriteLine("please provide a file path");
+                        return 1;
+                    }
+
+                    if (!urlOption.HasValue())
+                    {
+                        Console.WriteLine("please provide a url");
+                        return 1;
+                    }
+
+                    Console.WriteLine(fileOption.ValueName + ":" + fileOption.Value());
+                    Console.WriteLine(urlOption.ValueName + ":" + urlOption.Value());
+
+                    return 0;
+                });
+            });
+
+            app.Execute(args);
+            Console.ReadLine();
+            return 0;
+        }
+    }
+}
---
+++ src/JsonServer/Properties/AssemblyInfo.cs
@@ -0,0 +1,23 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("JsonServer")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Seek Limited")]
+[assembly: AssemblyProduct("JsonServer")]
+[assembly: AssemblyCopyright("Copyright © Seek Limited 2016")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("810e6b44-1378-416e-be11-1a716447c773")]
